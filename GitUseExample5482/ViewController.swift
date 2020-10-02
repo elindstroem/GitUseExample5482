ac713b1492974dae75137501cbe93a762e3c8935
Ella Lindstrom <ella.lindstroem.548@k12.friscoisd.org>
2020-10-02 09:12:31 -0500

    Initial Commit

---
+++ GitUseExample5482/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample5482
+//
+//  Created by Lindstrom, Ella on 10/2/20.
+//  Copyright © 2020 Lindstrom, Ella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
